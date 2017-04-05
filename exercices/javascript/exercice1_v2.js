20b84568eac4e172958dec31a0212b5c1d89ff7e
ballec <allec.benjamin@gmail.com>
2017-04-05 11:45:33 +0200

    Delete exercice1_v2.js

--- exercices/javascript/exercice1_v2.js
+++
@@ -1,6 +0,0 @@
-function calculatrice() {
- //lire le calcul a éffectuer
- var calcul=eval(document.calculette.calcul.value);
- //affichage du résultat
- document.calculette.resultat.value=calcul;
-}
